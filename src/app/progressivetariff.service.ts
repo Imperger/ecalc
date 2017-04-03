8ca2b68edb54878975debdf60726019768cb75d4
Imperger <mr.imperger@hotmail.com>
2017-04-03 13:39:03 +0300

    Изменение тарифов с 01.03.2017

--- src/app/progressivetariff.service.ts
+++ src/app/progressivetariff.service.ts
@@ -3,5 +3,5 @@ import {IProgressiveCost} from './progressivecost.interface';
 
 @Injectable()
 export class ProgressiveTariffService {
-    public costs: IProgressiveCost[] = [{cost: 71.4, limit: 100}, {cost: 129, limit: 600}, {cost: 163.8}];
+    public costs: IProgressiveCost[] = [{cost: 90, limit: 100}, {cost: 168}];
 }
